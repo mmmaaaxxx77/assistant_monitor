f9a77ee42b564193caf3f0c82a6389d5b95e4e96
johnny <mmmaaaxxx77@gmail.com>
2018-03-12 17:23:14 +0800

    no message

--- monitor/microservice/src/index.js
+++ monitor/microservice/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/css_reset.css';
-import 'sanitize.css';
-import './styles/index.css';
+import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
