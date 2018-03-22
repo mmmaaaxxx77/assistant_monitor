aca878a5931d67655d27afff69a95f064beb8cb1
johnny <mmmaaaxxx77@gmail.com>
2018-03-22 16:03:16 +0800

    no message

--- pig_bot/microservice/server/app.js
+++ pig_bot/microservice/server/app.js
@@ -135,21 +135,23 @@ const handleGameTask = function(data){
                 continue;
             }else if(value['last_time'] == offers[key]['end_time']*1000 &&
                 !cfull &&
-                now_st - value['last_alert'] > 1000*60*30) {
-                gameTaskCache.set(offers[key]['url'], {
-                    'last_time': offers[key]['end_time'] * 1000,
-                    'alert': true,
-                    'last_alert': now_st
-                });
-            }else if(value['last_time'] == offers[key]['end_time']*1000 &&
-                !cfull &&
                 now_st - value['last_alert'] < 1000*60*30){
+                console.log(now_st + "," + value['last_alert']);
                 gameTaskCache.set(offers[key]['url'], {
                     'last_time': offers[key]['end_time']*1000,
                     'alert': value['alert'],
                     'last_alert': value['last_alert']
                 });
                 continue;
+            }else if(value['last_time'] == offers[key]['end_time']*1000 &&
+                !cfull &&
+                now_st - value['last_alert'] > 1000*60*30) {
+                console.log(now_st + "," + value['last_alert']);
+                gameTaskCache.set(offers[key]['url'], {
+                    'last_time': offers[key]['end_time'] * 1000,
+                    'alert': true,
+                    'last_alert': now_st
+                });
             }else{
                 gameTaskCache.set(offers[key]['url'], {
                     'last_time': offers[key]['end_time']*1000,
